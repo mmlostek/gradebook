80c171c76a948102bf43b35254f04affe0b91f40
mmlostek <76187606+mmlostek@users.noreply.github.com>
2022-03-31 22:27:22 +0200

    19/24

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name, bool isWeighted):base(name, isWeighted)
+        {
+           Type = GradeBookType.Ranked;
+        }
+  
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+        public void GetLetterGrade(BaseGradeBook Student)
+        {
+           
+            var twentyPercent = Students.Count*0.2;
+            foreach(GradeBook.Student student in Students)
+            {
+                //grade = Grades(student);
+            }
+        }
+    }
+}
